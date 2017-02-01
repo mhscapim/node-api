2b5d9918f59abcab232a0269379918f100b9aa47
mhscapim <mhscapim@gmail.com>
2017-02-01 17:24:56 -0200

    CRUD dos recursos

--- libs/middlewares.js
+++ libs/middlewares.js
@@ -1,4 +1,12 @@
+const bodyParser = require('body-parser');
+
 module.exports = app => {
     app.set('port', process.env.PORT || 3000);
     app.set('json spaces', 4);
+    app.use(bodyParser.json());
+    app.use((req, res, next) => {
+        // Middleware de pré-execução das rotas
+        delete req.body.id;
+        next();
+    });
 };
